38852e6ba91b5d81b9b49655c2816352770e9dcc
roland aminu <aminuroland452@gmail.com>
2026-09-23 08:10:05 +0100

    secure fix

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -7,7 +7,7 @@ function setAuthCookie(res, userId) {
   res.cookie('token', token, {
     httpOnly: true,
     sameSite: 'lax',
-    secure: false,
+    secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 }
