f307a518570eac7450c5fd528388473b9d0243d4
roland aminu <aminuroland452@gmail.com>
2026-09-24 08:28:32 +0100

    d

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -199,6 +199,7 @@ export default function DashboardPage() {
                     </p>
                   </div>
                 </div>
+                <div>
                 <p
                   className={`font-semibold ${
                     tx.type === "deposit" ? "text-green-400" : "text-red-400"
@@ -207,6 +208,10 @@ export default function DashboardPage() {
                   {tx.type === "deposit" ? "+" : "-"}$
                   {Number(tx.amount).toLocaleString()}
                 </p>
+                <p
+                  className={tx.status == 'pending'? 'text-yellow-400' : tx.status == 'success'? 'text-green-400' : 'text-red-400'}
+                >{tx.status}</p>
+                </div>
               </motion.div>
             ))}
           </div>
